6cf0e17030841d4dbdc4659c8da45436ee50422e
Seth Randall <sethjrandall@gmail.com>
2024-02-18 15:02:21 -0500

    Add form to create books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,28 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+    @authors = Author.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def list
     books = Book.includes(:author).order("#{params[:column]} #{params[:direction]}")
     render(partial: 'books_table', locals: { books: books })
   end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author_id, :genre, :pages)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'books/index'
   get 'books/show'
+  get 'books/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
