2525df4578bed87d34e02a86b948cfd0446e383b
Seth Randall <sethjrandall@gmail.com>
2024-02-25 09:48:59 -0500

    Add new author form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,4 +5,23 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to new_book_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name)
+    end
 end
